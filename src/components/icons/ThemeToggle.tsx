f600470c077797cea619ce49e2c9152889272cef
Richard Bittencourt Mezzomo <richardmezzomo1@gmail.com>
2026-03-30 21:41:09 -0300

    feat: add theme provider and global styles

---
+++ src/components/icons/ThemeToggle.tsx
@@ -0,0 +1,24 @@
+// src/components/icons/ThemeToggleIcon.tsx
+export function ThemeToggleIcon({ size = 18 }: { size?: number }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <title>Toggle theme</title>
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+      <path d="M12 3l0 18" />
+      <path d="M12 9l4.65 -4.65" />
+      <path d="M12 14.3l7.37 -7.37" />
+      <path d="M12 19.6l8.85 -8.85" />
+    </svg>
+  )
+}
