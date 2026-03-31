af8eacd607434511a048453e0a76a69ed903a54e
Richard Bittencourt Mezzomo <richardmezzomo1@gmail.com>
2026-03-30 21:41:06 -0300

    chore: update project config and dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
